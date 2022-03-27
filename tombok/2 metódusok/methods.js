13e6aceeb968d2bb7214fa9daa3771a749d973bb
unknown <attila.kormos@gmail.com>
2022-03-27 18:56:03 +0200

    Tömbök létrehozása, módosítása, metódusok

--- tombok/2 metódusok/methods.js
+++ tombok/2 metódusok/methods.js
@@ -9,6 +9,13 @@ console.log(csapatok);
 csapatok.unshift('Diósgyyőr');
 console.log(csapatok);
 
+// Új elem hozzáadása a tömb x-edik indexeleméhez
+let x = 1; // Ez valójában a második pozíció
+csapatok.splice(x, 0, 'Kaposvár');
+console.log(csapatok);
+
+
+
 // Tömb utolsó elemének törlése - változóba írható a törölt elem értéke
 let torolt_elem = csapatok.pop();
 console.log(csapatok);
@@ -18,5 +25,15 @@ console.log(torolt_elem);
 torolt_elem = csapatok.shift();
 console.log(csapatok);
 console.log(torolt_elem);
+
+// Tömb x-edik indexelemének törlése
+x = 4; // Ez egy index, valójában az ötödik elemet jelöli
+let elemszam = 1; // Hány elem kerüljön törlésre a megadott indextől
+torolt_elem = csapatok.splice(x, elemszam);
+console.log(csapatok);
+console.log(torolt_elem); // Egy tömböt ad vissza egy elem esetén is
+
+
+
 
 
