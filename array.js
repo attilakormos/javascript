0ef9786fe48dfc68810e1c39961df9b66a5981c3
unknown <attila.kormos@gmail.com>
2022-03-27 11:16:43 +0200

    Tömbök létrehozása

--- array.js
+++ array.js
@@ -2,6 +2,7 @@
 // egyszerű: - bevásárlólista, - lottószámok, - rendetlen adatok 
 // összetett: - lejátszólisták, - hozzászólások, - tanulók
 
+
 // Üres bevásárlólista tömb létrehozása
 let bevasarlolista = [];
 
@@ -18,10 +19,15 @@ console.log(bevasarlolista.length);
 let utolso = bevasarlolista.length - 1 
 console.log(bevasarlolista[utolso]);
 
+// Nem létező indexelemre hivatkozás eredménye undefined
+console.log(bevasarlolista[4]);
+
+
 // Lottószámok tömb létrehozása - második módszer
 let lotto = new Array(2, 7, 25, 36, 71);
 
 console.log(lotto);
+
 
 // Rendetlen adatok tömb létrehozása - eltérú elemtípusok
 let rendetlen = ['gumikacsa', 25, 721, "zokni", 79.6, true];
