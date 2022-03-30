1cb8fec6cad45e58b697b4ffbff827e42ef14842
attilakormos <attila.kormos@gmail.com>
2022-03-30 09:43:35 +0200

    Tömb elemeinek kezelése

--- tombok/2 elem hozzáadása, törlése/methods.js
+++ tombok/2 elem hozzáadása, törlése/methods.js
@@ -2,12 +2,16 @@
 let csapatok = ['Ferencváros', 'Videoton', 'Debrecen','Honvéd','MTK','Paks'];
 
 // ELEMEK HOZZÁADÁSA
-// Új elem hozzáadása a tömb végéhez - tomb.length index is ugyanez 
+// Egy új elem hozzáadása a tömb végéhez - tomb.length index is ugyanez 
 csapatok.push('Vasas');
 console.log(csapatok);
 
-// Új elem hozzáadása a tömb elejéhez
-csapatok.unshift('Diósgyyőr');
+// Új elemek hozzáadása a tömb végéhez
+csapatok.push('Stadler', 'Gyirmót', 'REAC'); // Több paraméterrel is használható
+console.log(csapatok);
+
+// Egy új elem hozzáadása a tömb elejéhez
+csapatok.unshift('Diósgyyőr'); // Több paraméterrel is használható
 console.log(csapatok);
 
 // Új elem hozzáadása a tömb x-edik indexeleméhez
@@ -33,13 +37,18 @@ console.log(csapatok);
 console.log(torolt_elem);
 
 // Tömb x-edik indexelemének törlése
-x = 4; // Ez egy index, valójában az ötödik elemet jelöli
+x = 3; // Ez valójában a negyedik elemet jelöli
 let elemszam = 1; // Hány elem kerüljön törlésre a megadott indextől
 torolt_elem = csapatok.splice(x, elemszam);
 console.log(csapatok);
 console.log(torolt_elem); // Egy tömböt ad vissza egy elem esetén is
 
-
+// Tömb több elemének törlése
+x = 0; // Az első elemtől kezdve
+elemszam = 2; // Két elemet
+torolt_elem = csapatok.splice(x, elemszam);
+console.log(csapatok);
+console.log(torolt_elem);
 
 
 
